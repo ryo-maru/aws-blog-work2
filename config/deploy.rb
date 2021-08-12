b24a6c80b7f1359e30844572e2d9f5b9f6a5abd7
maruyamaryo <t.t.uncle0803@gmail.com>
2021-08-12 16:56:31 +0900

    aaaaa

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'https://github.com/ryo-maru/aws-blog-work2.git'
 # deployするブランチ。デフォルトでmainを使用している場合、masterをmainに変更してください。
 set :branch, ENV['BRANCH'] || 'master'
 # deploy先のディレクトリ。
-set :deploy_to, '/var/www/achieve'
+set :deploy_to, '/var/www/aws-blog-app'
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}
